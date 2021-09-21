ffaa328ac39cbab3b8a8465530e796517ee7d0e9
Osir-is <78626961+Osir-is@users.noreply.github.com>
2021-09-21 20:31:20 +0100

    fixed the update and delete and get requests to ls

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,49 @@
 const storageItem = (function () {
     // public methods
     return {
-      storeItem: function (item) {
-        let items;
-        //check if any items in local storage
-        if (localStorage.getItem("items") === null) {
-          items = [];
-          //push new items
-          items.push(item);
+      storeItem: function (MealItem) {
+        let mealTimes;
+        //check if any mealTimes in local storage
+        if (localStorage.getItem("mealTimes") === null) {
+          mealTimes = [];
+          //push new mealTimes
+          mealTimes.push(MealItem);
           //set local storage
-          localStorage.setItem("items", JSON.stringify(items));
+          localStorage.setItem("mealTimes", JSON.stringify(mealTimes));
         } else {
-          items = JSON.parse(localStorage.getItem("items"));
-          items.push(item);
-          localStorage.setItem("items", JSON.stringify(items));
+          mealTimes = JSON.parse(localStorage.getItem("mealTimes"));
+          mealTimes.push(MealItem);
+          localStorage.setItem("mealTimes", JSON.stringify(mealTimes));
         }
-      }}})
+      },getItemFromStorage: function () {
+        let mealTimes;
+        if (localStorage.getItem("mealTimes") === null) {
+          mealTimes = [];
+        } else {
+          mealTimes = JSON.parse(localStorage.getItem("mealTimes"));
+        }
+        return mealTimes;
+      },
+      updatemealTimestorage: function (updatedItem) {
+        let mealTimes = JSON.parse(localStorage.getItem("mealTimes"));
+        mealTimes.forEach((MealItem, index) => {
+          if (updatedItem.id === MealItem.id) {
+            mealTimes.splice(index, 1, updatedItem);
+          }
+        });
+        localStorage.setItem("mealTimes", JSON.stringify(mealTimes));
+      },
+      deletemealTimestorage: function (itemToDeleteID) {
+        let mealTimes = JSON.parse(localStorage.getItem("mealTimes"));
+        mealTimes.forEach((MealItem, index) => {
+          if (itemToDeleteID === MealItem.id) {
+            mealTimes.splice(index, 1);
+          }
+        });
+        localStorage.setItem("mealTimes", JSON.stringify(mealTimes));
+      },
+      removeAllmealTimes: function () {
+        localStorage.removeItem("mealTimes");
+      },
+    };
+  })();
